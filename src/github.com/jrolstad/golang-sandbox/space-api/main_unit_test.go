37cd42ec931b36667cc0aa8dc75a92bc4f628334
Josh Rolstad <jrolstad@gmail.com>
2021-12-18 16:42:59 +0000

    Adding separation between unit and integration tests

---
+++ src/github.com/jrolstad/golang-sandbox/space-api/main_unit_test.go
@@ -0,0 +1,26 @@
+//go:build unit
+// +build unit
+
+package main
+
+import "testing"
+
+// Based on the blog post at https://blog.alexellis.io/golang-writing-unit-tests/
+func Test_GetAstronauts_NoInputs_ReturnsAstronautData(t *testing.T) {
+	result := GetAstronauts()
+
+	AssertDataReceived(t, result)
+	AssertAstronautCountIsLargerThanZero(t, result)
+}
+
+func AssertDataReceived(t *testing.T, data people) {
+	if data == (people{}) {
+		t.Error("No data received")
+	}
+}
+
+func AssertAstronautCountIsLargerThanZero(t *testing.T, data people) {
+	if data.Number <= 0 {
+		t.Error("No Astronaut data received")
+	}
+}
